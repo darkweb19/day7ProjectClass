3165f4149021fbc3056389f8a32c9ead71e30f38
darkweb19 <sujanstha3220@gmail.com>
2023-03-17 12:29:15 +0545

    first commit

---
+++ User.js
@@ -0,0 +1,70 @@
+//!Task
+// class User {
+// 	static count = 0;
+// 	constructor(name, email) {
+// 		this.name = name;
+// 		this.email = email;
+// 		User.count++;
+// 	}
+// }
+
+// const ram = new User("Ram", "ram@gmail.com");
+// console.log(ram);
+
+// const hari = new User("Hari", "hari@gmail.com");
+// console.log(hari);
+// const sujan = new User("Sujan", "sujanari@gmail.com");
+// console.log(sujan);
+
+// console.log(User.count);
+
+//!Task
+const uuid = require("uuid").v4;
+
+//static is used to share the data
+class User {
+	static #data = [];
+	// static #data = [
+	// 	{
+	// 		id: 1, //assign an unique id
+	// 		name: " Sujan",
+	// 	},
+	// 	{
+	// 		id: 2, //assign an unique id
+	// 		name: "Prakanda",
+	// 	},
+	// ];
+
+	static find() {
+		return User.#data;
+	}
+
+	#temp = {
+		id: uuid(),
+	};
+
+	constructor(data) {
+		if (!data.name || !data.email) {
+			throw new Error("Reqired data are missing");
+		}
+		this.#temp.name = data.name;
+		this.#temp.email = data.email;
+	}
+
+	save() {
+		User.#data.push(this.#temp);
+	}
+
+	static deleteById(id) {
+		User.#data = User.#data.filter((user) => {
+			return user.id != id;
+		});
+	}
+}
+
+// const ram = new User("Ram");
+// ram.save();
+// User.deleteById(2);
+// console.log(User.find());
+
+module.exports = User;
---
+++ index.js
@@ -0,0 +1,76 @@
+// const express = require("express");
+// const uuid = require("uuid").v4;
+// const app = express();
+
+// app.use(express.urlencoded({ extended: true }));
+// let users = [
+// 	{ id: uuid(), name: "Sujan", age: 12 },
+// 	{ id: uuid(), name: "prakanda", age: 13 },
+// ];
+
+// //get method to see the users
+// app.get("/users", (req, res) => {
+// 	// console.log(users);
+// 	res.send(users);
+// });
+
+// //post method to add the user from body of the page
+// app.post("/add-user", (req, res) => {
+// 	if (req.body.name && req.body.age) {
+// 		users.push({
+// 			id: uuid(),
+// 			name: req.body.name,
+// 			age: req.body.age,
+// 		});
+// 		res.send("User added");
+// 	} else {
+// 		res.send("Please provide name");
+// 	}
+// });
+// //get method to delete a user
+// app.get("/delete-user", (req, res) => {
+// 	if (req.query.id) {
+// 		users = users.filter((user) => {
+// 			return user.id != req.query.id;
+// 		});
+// 		res.send("user deleted");
+// 	} else {
+// 		res.send("please provide an id");
+// 	}
+// });
+
+// app.listen(3000, () => {
+// 	console.log("Server is at post 3000 :");
+// });
+
+//!task  Implementing from class
+
+const express = require("express");
+const app = express();
+const User = require("./User");
+app.use(express.urlencoded({ extended: true }));
+
+//get user
+app.get("/user", (req, res) => {
+	res.send(User.find());
+});
+
+//add user
+app.post("/add-user", (req, res) => {
+	try {
+		const user = new User(req.body);
+		user.save();
+		res.send("User added");
+	} catch (err) {
+		res.send(err.message);
+	}
+});
+
+//delete user
+app.get("/delete-user", (req, res) => {
+	User.deleteById(req.query.id);
+	res.send("User deleted");
+});
+app.listen(3000, () => {
+	console.log("Server started at 3000");
+});
